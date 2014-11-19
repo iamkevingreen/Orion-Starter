7e64e51df3a15a6a796514add4f8a2e2a238453d
Kevin Green <thedudem4n@gmail.com>
2014-11-18 19:27:58 -0500

    added some simple site settings to the dictionary

---
+++ schema/dictionary/site.js
@@ -0,0 +1,23 @@
+orion.dictionary.addDefinition('siteName', 'site', {
+  type: String,
+  label: "Site Name",
+  optional: true,
+});
+
+orion.dictionary.addDefinition('siteDescription', 'site', {
+  type: String,
+  label: "Short Blurp about the site",
+  optional: true,
+});
+
+orion.dictionary.addDefinition('videoURL', 'site', {
+  type: String,
+  label: "Video URL",
+  optional: true,
+});
+
+orion.dictionary.addDefinition('headerImage', 'site', {
+  type: orion.attributes.image,
+  label: "Header Fallback Image",
+  optional: true,
+});
