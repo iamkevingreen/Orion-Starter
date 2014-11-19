f45ddc76c2a28d4744ae25619e297504d2b31629
Kevin Green <kevin@swedenunlimited.com>
2014-11-19 17:51:39 -0500

    updated work schema for better filtering

--- schema/work.js
+++ schema/work.js
@@ -46,57 +46,75 @@ orion.addEntity('works', {
 	// 	type: [orion.attributes.image],
 	// 	label: "Detail Images"
 	// },
-  toolsUsed: {
-    type: [String],
-    autoform: {
-      type: "select-checkbox",
-      options: function () {
-        return [
-          {label: "GitHub", value: 'github'},
-          {label: "Laravel", value: 'laravel'},
-          {label: "Ruby on Rails", value: 'rails'},
-          {label: "Heroku", value: 'heroku'},
-          {label: "Apache Solr", value: 'apache-solr'},
-          {label: "Amazon Web Services", value: 'aws'},
-          {label: "PHP", value: 'php'},
-          {label: "Javascript", value: 'javascript'},
-          {label: "Stripe", value: 'stripe'},
-          {label: "Paypal", value: 'paypal'},
-          {label: "Drupal", value: 'drupal'},
+	languesUsed: {
+		type: [String],
+		autoform: {
+			type: "select-checkbox",
+			options: function () {
+				return [
+					{label: "Ruby on Rails", value: 'rails'},
+					{label: "PHP", value: 'php'},
+					{label: "Javascript", value: 'javascript'},
+					{label: "Drupal", value: 'drupal'},
+					{label: "Meteor", value: 'meteor'},
+					{label: "Python", value: 'python'},
+					{label: "Backbone", value: 'backbone'},
+					{label: "Node", value: 'node'},
+					{label: "Angular", value: 'angular'},
+				];
+			}
+		}
+	},
+	toolsUsed: {
+		type: [String],
+		autoform: {
+			type: "select-checkbox",
+			options: function () {
+				return [
+					{label: "GitHub", value: 'github'},
+					{label: "Laravel", value: 'laravel'},
+					{label: "Ruby on Rails", value: 'rails'},
+					{label: "Heroku", value: 'heroku'},
+					{label: "Apache Solr", value: 'apache-solr'},
+					{label: "Amazon Web Services", value: 'aws'},
+					{label: "PHP", value: 'php'},
+					{label: "Javascript", value: 'javascript'},
+					{label: "Stripe", value: 'stripe'},
+					{label: "Paypal", value: 'paypal'},
+					{label: "Drupal", value: 'drupal'},
 					{label: "Bower", value: 'bower'},
-					{label: "Bower", value: 'bower'},
-          {label: "jQuery", value: 'jquery'},
-          {label: "Grunt", value: 'grunt'},
-          {label: "Stellar", value: 'stellar'},
-          {label: "Meteor", value: 'meteor'},
-          {label: "PhoneGap", value: 'phonegap'},
-          {label: "Wordpress", value: 'wordpress'},
-          {label: "Themosis", value: 'themosis'},
-          {label: "Python", value: 'python'},
-          {label: "D3", value: 'd3'},
-          {label: "Timber", value: 'timber'},
-          {label: "Advanced Custom Fields", value: 'acf'},
-          {label: "Expression Engine", value: 'expression-engine'},
-          {label: "Shopify", value: 'shopify'},
-          {label: "Tumblr", value: 'tumblr'},
-          {label: "BigCommerce", value: 'bigcommerce'},
-          {label: "SASS", value: 'sass'},
-          {label: "Backbone", value: 'backbone'},
-          {label: "Node", value: 'node'},
-          {label: "Express", value: 'express'},
+					{label: "jQuery", value: 'jquery'},
+					{label: "Grunt", value: 'grunt'},
+					{label: "Stellar", value: 'stellar'},
+					{label: "Meteor", value: 'meteor'},
+					{label: "PhoneGap", value: 'phonegap'},
+					{label: "Wordpress", value: 'wordpress'},
+					{label: "Themosis", value: 'themosis'},
+					{label: "Python", value: 'python'},
+					{label: "D3", value: 'd3'},
+					{label: "Timber", value: 'timber'},
+					{label: "Advanced Custom Fields", value: 'acf'},
+					{label: "Expression Engine", value: 'expression-engine'},
+					{label: "Shopify", value: 'shopify'},
+					{label: "Tumblr", value: 'tumblr'},
+					{label: "BigCommerce", value: 'bigcommerce'},
+					{label: "SASS", value: 'sass'},
+					{label: "Backbone", value: 'backbone'},
+					{label: "Node", value: 'node'},
+					{label: "Express", value: 'express'},
 					{label: "MySQL", value: 'mysql'},
 					{label: "Postgres", value: 'postgresql'},
 					{label: "Redis", value: 'redis'},
-          {label: "MongoDB", value: 'mongodb'},
-          {label: "Angular", value: 'angular'},
-          {label: "KeystoneJS", value: 'keystonejs'},
-          {label: "LESS", value: 'less'},
-          {label: "Spree", value: 'spree'},
-          {label: "Gulp", value: 'gulp'}
-        ];
-      }
-    }
-  },
+					{label: "MongoDB", value: 'mongodb'},
+					{label: "Angular", value: 'angular'},
+					{label: "KeystoneJS", value: 'keystonejs'},
+					{label: "LESS", value: 'less'},
+					{label: "Spree", value: 'spree'},
+					{label: "Gulp", value: 'gulp'}
+				];
+			}
+		}
+	},
 }, {
 	sidebarName: 'Works',
 	pluralName: 'Works',
