a749cf440a23e39977705808599fd4ea16655e82
Kevin Green <kevin@swedenunlimited.com>
2014-12-01 15:28:12 -0500

    some additional post custom styling

--- client/views/blog/blog.js
+++ client/views/blog/blog.js
@@ -1,8 +1,5 @@
 Template.posts.helpers({
     posts: function () {
-      console.log('yo');
-      all = orion.entities.posts.collection.find({}).fetch();
-      console.log(all);
       return orion.entities.posts.collection.find().fetch();
     }
 });
