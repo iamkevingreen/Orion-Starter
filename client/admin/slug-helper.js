85dd1c690bb259b10b455a6bcab015a1a7d76c8d
Kevin Green <kevin@swedenunlimited.com>
2014-12-05 10:17:48 -0500

    update slug helper

--- client/admin/slug-helper.js
+++ client/admin/slug-helper.js
@@ -1,5 +1,5 @@
 Template.adminEntitiesCreate.events({
-  'submit form': function(e) {
+  'click .btn': function(e) {
     str = $('input[name="title"]').val();
     str = str.replace(/[^a-zA-Z0-9\s]/g,"");
     str = str.toLowerCase();
