6da4318737c36a6ae2b54128681dc895239b0406
Kevin Green <kevin@swedenunlimited.com>
2014-12-05 16:56:30 -0500

    404s redirect to the index

--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 Router.configure({
   loadingTemplate: 'adminLoading',
+  notFoundTemplate: 'notFound',
   waitOn: function() {
     return [orion.subs.subscribe('dictionary'), orion.subs.subscribe('entity', 'works'), orion.subs.subscribe('entity', 'posts'), orion.subs.subscribe('entity', 'pages')];
   }
@@ -79,6 +80,13 @@ Router.map(function() {
       return [orion.subs.subscribe('entity', 'pages', {slug: this.params.slug})]
     },
     data: function() {
+      page = orion.entities.pages.collection.findOne({slug: this.params.slug});
+      var response = this.response;
+      console.log(response);
+      if (page == undefined) {
+        this.redirect('base');
+        return null;
+      }
       return orion.entities.pages.collection.findOne({slug: this.params.slug});
     }
   })
