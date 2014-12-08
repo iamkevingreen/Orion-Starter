5fd2038f915acee234f3c5d73e5bb5cdc3dc3ca2
Kevin Green <kevin@swedenunlimited.com>
2014-12-08 15:58:51 -0500

    hide slug and label on creation clean up the helper

--- client/admin/slug-helper.js
+++ client/admin/slug-helper.js
@@ -1,10 +1,16 @@
 Template.adminEntitiesCreate.events({
   'click .btn': function(e) {
     str = $('input[name="title"]').val();
+    if (str == undefined) {
+      str = $('input[name="projectName"]').val();
+    }
     str = str.replace(/[^a-zA-Z0-9\s]/g,"");
     str = str.toLowerCase();
     str = str.replace(/\s/g,'-');
     $('input[name="slug"]').val(str);
-    console.log('yo');
   }
 });
+
+Template.adminEntitiesCreate.rendered = function() {
+  $('input[name="slug"]').parent().hide();
+}
